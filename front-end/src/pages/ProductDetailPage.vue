c038ba91dfc624f1ec9ba96d8199adeed52da27e
yushan <yushanfans2233@qq.com>
2024-06-16 18:46:35 +0800

    Creating a product detail page

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,9 +1,23 @@
 <template>
-    <h1>产品信息界面</h1>
+    <div class="img-wrap">
+      <img :src="product.imageName">
+    </div>
+    <div class="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 class="price">{{ product.price }}</h3>
+      <button class="add-to-cart">加入购物车</button>
+    </div>
   </template>
   
   <script>
+  import { products } from '../temp-data'
+
   export default {
     name: "ProductDetailPage",
+    data() {
+      return {
+        product: products.find(product => product.id === this.$route.params.productId),
+        }
+    },
   }
   </script>
