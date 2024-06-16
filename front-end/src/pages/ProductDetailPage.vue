db0c43076ea3a335bfbcb6863f08eecdc80acca4
yushan <yushanfans2233@qq.com>
2024-06-16 20:05:23 +0800

    Creating a 404 page in Vue

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,4 +1,5 @@
 <template>
+  <div v-if="product">
     <div class="img-wrap">
       <img :src="product.imageName">
     </div>
@@ -7,17 +8,25 @@
       <h3 class="price">{{ product.price }}</h3>
       <button class="add-to-cart">加入购物车</button>
     </div>
-  </template>
+  </div>
+  <div v-else>
+    <NotFoundPage />
+  </div>
+</template>
   
-  <script>
-  import { products } from '../temp-data'
+<script>
+import { products } from '../temp-data'
+import NotFoundPage from './NotFoundPage.vue'
 
-  export default {
-    name: "ProductDetailPage",
-    data() {
-      return {
-        product: products.find(product => product.id === this.$route.params.productId),
-        }
-    },
-  }
-  </script>
+export default {
+  name: "ProductDetailPage",
+  data() {
+    return {
+      product: products.find(product => product.id === this.$route.params.productId),
+      }
+  },
+  components: {
+    NotFoundPage
+  },
+}
+</script>
