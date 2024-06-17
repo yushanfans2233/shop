6f3c189bb8e419576a3794588451d293cbe9e4bf
yushan <yushanfans2233@qq.com>
2024-06-17 09:31:08 +0800

    Using modular components

---
+++ front-end/src/components/ProductsList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="grid-wrap">
+        <div class="product-item" v-for="product in products" :key="product.id">
+          <img :src="product.imageName" />
+          <h3 class="product-name">{{ product.name }}</h3>
+          <p class="product-price">{{ product.price }}</p>
+          <router-link :to="'/products/' + product.id">
+            <button>查看详细信息</button>
+          </router-link>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "ProductsList",
+    props: ['products'],
+}
+</script>
--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -1,22 +1,17 @@
 <template>
   <h1>产品界面</h1>
-  <div class="grid-wrap">
-    <div class="product-item" v-for="product in products" :key="product.id">
-      <img :src="product.imageName" />
-      <h3 class="product-name">{{ product.name }}</h3>
-      <p class="product-price">{{ product.price }}</p>
-      <router-link :to="'/products/' + product.id">
-        <button>查看详细信息</button>
-      </router-link>
-    </div>
-  </div>
+  <ProductsList :products="products"/>
 </template>
 
 <script>
+import ProductsList from '../components/ProductsList.vue'
 import { products } from '../temp-data';
 
 export default {
   name: "ProductPage",
+  components: {
+    ProductsList,
+  },
   data() {
     return {
       products,
