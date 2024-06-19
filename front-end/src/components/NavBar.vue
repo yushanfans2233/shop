31ddcf4f6400236a9cc6690aa293566ca39a2cff
yushan <yushanfans2233@qq.com>
2024-06-19 22:15:03 +0800

    Signing the user out

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -1,26 +1,38 @@
 <template>
-<div class="nav-bar">
+  <div class="nav-bar">
     <router-link to="/products" class="products-link">
-        <div class="logo-wrap">
-            <img :src="logo">
-        </div>
+      <div class="logo-wrap">
+        <img :src="logo">
+      </div>
     </router-link>
 
-    <router-link to="/cart" class="cart-link">
-        <button>购物车</button>
-    </router-link>
-</div>
+    <div class="nav-buttons-wrap">
+      <button @click="signOut" v-if="user">Sign Out</button>
+      <router-link to="/cart">
+        <button>Shopping Cart</button>
+      </router-link>
+    </div>
+
+  </div>
 </template>
 
 <script>
+import {getAuth, signOut} from 'firebase/auth';
 import logo from '../assets/logo-hexagon.svg';
 
 export default {
-    name: "NarBar",
-    data() {
-        return {
-            logo,
-        }
+  name: "NavBar",
+  props: ['user'],
+  data() {
+    return {
+      logo,
     }
+  },
+  methods: {
+    signOut() {
+      const auth = getAuth();
+      signOut(auth);
+    }
+  }
 }
 </script>
