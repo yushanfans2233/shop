dd8bd75f8a3b90a9428bb61606173aaa49445149
yushan <yushanfans2233@qq.com>
2024-06-19 21:28:26 +0800

    Creating a Firebase project

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -6,6 +6,19 @@ import ShoppingCartPage from './pages/ShoppingCartPage.vue';
 import ProductsPage from './pages/ProductsPage.vue';
 import ProductDetailPage from './pages/ProductDetailPage.vue';
 import NotFoundPage from './pages/NotFoundPage.vue'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9Zzl0S-e_Z_C3LmWa_NHRXJWrP_XaXME",
+  authDomain: "vue-site-8cff4.firebaseapp.com",
+  projectId: "vue-site-8cff4",
+  storageBucket: "vue-site-8cff4.appspot.com",
+  messagingSenderId: "534164477039",
+  appId: "1:534164477039:web:95039f94461c045ad2e99a"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 createApp(App)
 .use(VueRouter.createRouter({
